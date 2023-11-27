27f54131f22863d6c28b41c4642294f0741810de
Shekhar Dangi <arpitt682@gmail.com>
2023-11-27 23:36:12 +0530

    login message pop

--- src/app/auth/page.jsx
+++ src/app/auth/page.jsx
@@ -6,6 +6,7 @@ import InputText from "@/components/InputText/InputText";
 import InputBox from "@/components/InputBox/InputBox";
 import Button from "@/components/Button/Button";
 import Image from "next/image";
+import { ToastContainer, toast } from "react-toastify";
 
 const LoginPage = () => {
   const { status } = useSession();
@@ -21,22 +22,25 @@ const LoginPage = () => {
   }
   // signIn("google")
   return (
-    <div className={styles.topContainer}>
-      <div className={styles.container}>
-        <div className={`${styles.webHead} f1half tNav`}>Welcome Back</div>
-        <div className={styles.loginMet}>
-          <span
-            onClick={() => signIn("google")}
-            className={styles.authItemContainer}
-          >
-            <img
-              alt="google"
-              src="/google.png"
-              className={`${styles.authItem}`}
-            ></img>
-            <p className="f09">Continue With Google</p>
-          </span>
-          {/* <span className={styles.authItemContainer}>
+    <>
+      {" "}
+      <ToastContainer position="top-right" autoClose={5000} theme="light" />
+      <div className={styles.topContainer}>
+        <div className={styles.container}>
+          <div className={`${styles.webHead} f1half tNav`}>Welcome Back</div>
+          <div className={styles.loginMet}>
+            <span
+              onClick={() => signIn("google")}
+              className={styles.authItemContainer}
+            >
+              <img
+                alt="google"
+                src="/google.png"
+                className={`${styles.authItem}`}
+              ></img>
+              <p className="f09">Continue With Google</p>
+            </span>
+            {/* <span className={styles.authItemContainer}>
             <img
               alt="github"
               src="/github.png"
@@ -52,21 +56,36 @@ const LoginPage = () => {
               style={{ color: "#000" }}
             ></img>
           </span> */}
-        </div>
-        <form className={styles.form} action="">
-          <InputText label={"USERNAME"} name={"username"} />
-          <InputText label={"PASSWORD"} name={"password"} />
-          <div className={styles.down}>
-            <div className="f07">
-              <InputBox />
-              Keep me signed in
-            </div>
-            <span className="f07 link">New User?</span>
           </div>
-          <Button text={"Login"} color={"white"} backCol={"#4A8076"} />
-        </form>
+          <form className={styles.form} action="">
+            <InputText label={"USERNAME"} name={"username"} />
+            <InputText label={"PASSWORD"} name={"password"} />
+            <div className={styles.down}>
+              <div className="f07">
+                <InputBox />
+                Keep me signed in
+              </div>
+              <span
+                onClick={() => {
+                  toast("Try logging in with google!");
+                }}
+                className="f07 link"
+              >
+                New User?
+              </span>
+            </div>
+            <Button
+              onClick={() => {
+                toast("Try logging in with google");
+              }}
+              text={"Login"}
+              color={"white"}
+              backCol={"#4A8076"}
+            />
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
